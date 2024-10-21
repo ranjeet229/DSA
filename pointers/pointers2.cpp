756a181b8e31e9387dfa0ecba6b7b404168cede4
Ranjeet <kiranjeetkr80@gmail.com>
2024-10-21 17:12:40 +0530

    thrid

--- pointers/pointers2.cpp
+++ pointers/pointers2.cpp
@@ -92,3 +92,17 @@
 
 //     return 0;
 // }
+
+                        // Question on double pointers 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int first=8;
+    int second=11;
+    int *third = &second;
+    first =*third;
+    *third =*third+2;
+    cout<<first<<" "<<second<<endl;
+    return 0;
+}
