ebf999fab2386471eefc1a0817489c4676bb0178
Ranjeet <kiranjeetkr80@gmail.com>
2024-10-21 16:48:48 +0530

    second

--- pointers/pointers2.cpp
+++ pointers/pointers2.cpp
@@ -46,21 +46,24 @@
 // }
 
 
-#include<iostream>
-using namespace std;
+            // double pointers 
 
-void update(int **p2){
-    // p=p+1; //no change in any value 
 
-    //*p2= *p2+1; //change hoga 
+// #include<iostream>
+// using namespace std;
 
-    **p2=**p2+1;
-}
+// void update(int **p2){
+//     // p=p+1; //no change in any value 
 
-int main(){
-    int i=5;
-    int*p=&i;
-    int**p2=&p;
+//     //*p2= *p2+1; //change hoga 
+
+//     **p2=**p2+1;
+// }
+
+// int main(){
+//     int i=5;
+//     int*p=&i;
+//     int**p2=&p;
 
     // cout<<"address of p is : "<<p<<endl;
     // //cout<<&p<<endl;
@@ -78,14 +81,14 @@ int main(){
     // cout<<&p<<endl;
     // cout<<p2<<endl; 
 
-    cout<<"before "<<i<<endl;
-    cout<<"before "<<p<<endl;
-    cout<<"before "<<p2<<endl;
+//     cout<<"before "<<i<<endl;
+//     cout<<"before "<<p<<endl;
+//     cout<<"before "<<p2<<endl;
 
-    update(p2);
-    cout<<"after "<<i<<endl;
-    cout<<"after "<<p<<endl;
-    cout<<"after "<<p2<<endl;
+//     update(p2);
+//     cout<<"after "<<i<<endl;
+//     cout<<"after "<<p<<endl;
+//     cout<<"after "<<p2<<endl;
 
-    return 0;
-}
+//     return 0;
+// }
