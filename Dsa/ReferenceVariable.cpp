4096cdd7e838eaa6ad2e707e5593fb98c39ea7a5
Ranjeet <kiranjeetkr80@gmail.com>
2024-11-05 21:58:02 +0530

    fifth

--- Dsa/ReferenceVariable.cpp
+++ Dsa/ReferenceVariable.cpp
@@ -33,15 +33,99 @@
 
             //heap memory
 
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     char ch='q';
+
+//     cout<<sizeof(ch)<<endl;
+//     char*c=&ch;
+//     cout<<sizeof(c)<<endl;
+
+//     return 0;
+// }
+
+
+                //Dynamic array in 2D array
+
+// 1 for same output
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//     int n;
+//     cin>>n;
+
+//     int** arr= new int*[n]; // creating 2d array in dynamic form 
+
+//     //creation a 2d array
+//     for(int i=0;i<n;i++){
+//         arr[i]=new int[n];
+//     }
+
+//     //taking input
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n;j++){
+//             cin>>arr[i][j];
+//         }
+//     }
+//     cout<<endl;
+
+//     //print output
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n;j++){
+//             cout<<arr[i][j]<<" ";
+//         }cout<<endl;
+//     }
+// }
+
+
+
+// for different row nad column 
+
 #include<iostream>
 using namespace std;
 
 int main(){
-    char ch='q';
 
-    cout<<sizeof(ch)<<endl;
-    char*c=&ch;
-    cout<<sizeof(c)<<endl;
+    int row;
+    cin>>row;
+
+    int col;
+    cin>>col;
+
+
+    //creation a 2d array
+    int** arr= new int*[row]; // creating 2d array in dynamic form 
+
+    for(int i=0;i<row;i++){
+        arr[i]=new int[col];
+    }
+
+    //taking input
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<endl;
+
+    //print output
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cout<<arr[i][j]<<" ";
+        }cout<<endl;
+    }
+
+    //relasing array
+    for(int i=0;i<row;i++){
+        delete[] arr[i];
+    }
+
+    delete []arr;
 
     return 0;
 }
