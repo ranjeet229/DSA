0e88ec9c9b33e194f637173cd2687a8d29ae8f8a
Ranjeet <kiranjeetkr80@gmail.com>
2025-01-15 16:41:25 +0530

    sec

--- Dsa/prime.cpp
+++ Dsa/prime.cpp
@@ -79,3 +79,26 @@
 //     }
 //};
 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n){
+    if(n<=1) return 0;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0) return 0;
+    }
+    return 1;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    if(isPrime(n)){
+        cout<<"is a prime number "<<endl;
+    }
+    else{
+        cout<<"is not a prime number "<<endl;
+    }
+    return 0;
+}
